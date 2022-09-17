75e5be7c49dff63d67cf93c2aa35504568426b1b
mhlizc <85056828+mhlizc@users.noreply.github.com>
2022-09-18 00:01:56 +0800

    20220918
    20220918 加上 瘦子 iu  林俊傑  麋先生 宇宙人 告五人

--- scripts/playlist.js
+++ scripts/playlist.js
@@ -1,392 +1,107 @@
 ﻿trackList = [
 	{
-		name:"U R",
-		artist:"太妍",
-		src:"music/y2mate.com - Lyrics   太妍TAEYEON  UR  中字.mp3"
+		name:"半山腰",
+		artist:"ZIC",
+		src:"music/Zic子晨 - 半山腰「不過是上山的人 怎麼嘲笑下山的神 這世界最不缺的就是天分」【動態歌詞PinyinLyrics】♪.mp3"
 	},
 	{
-		name:"四季",
-		artist:"太妍",
-		src:"music/y2mate.com - 韓中字太妍 태연 TAEYEON  四季 사계 Four Seasons.mp3"
+		name:"那些你很冒險的夢",
+		artist:"林俊傑 JJ Lin",
+		src:"music/yt1s.com - 林俊傑 JJ Lin  那些你很冒險的夢 Those Were The Days 官方完整 HD 高畫質版 MV.mp3"
 	},
 	{
-		name:"Fine",
-		artist:"太妍",
-		src:"music/y2mate.com - TAEYEON 태연 Fine MV.mp3"
+		name:"CHANGE",
+		artist:"瘦子ESO",
+		src:"music/yt1s.com - 瘦子ESOCHANGEOfficial Music Video5K.mp3"
 	},
 	{
-		name:"I",
-		artist:"太妍",
-		src:"music/y2mate.com - TAEYEON 태연 I feat Verbal Jint MV.mp3"
+		name:"Good Day好日子",
+		artist:"IU",
+		src:"music/yt1s.com - IU  Good Day好日子華納official HD 高畫質官方中字版.mp3"
 	},
 	{
-		name:"GEMINI",
-		artist:"太妍",
-		src:"music/y2mate.com - 유희열의 스케치북  태연  쌍둥이자리20160219.mp3"
+		name:"披星戴月的想你",
+		artist:"告五人",
+		src:"music/yt1s.com - 告五人 Accusefive 披星戴月的想你Official Music Video.mp3"
 	},
 	{
-		name:"Blue",
-		artist:"太妍",
-		src:"music/y2mate.com - DJ티비씨 태연  Blue  비긴어게인3 DJ티비씨.mp3"
+		name:"長成什麼樣子算愛情",
+		artist:"麋先生",
+		src:"music/yt1s.com - 麋先生 MIXER  長成什麼樣子算愛情 LoveLovedLoving  戲劇版MV華燈初上插曲.mp3"
 	},
 	{
-		name:"I Blame On You",
-		artist:"太妍",
-		src:"music/y2mate.com - I Blame On You.mp3"
+		name:"想把你拍成一部電影",
+		artist:"宇宙人",
+		src:"music/yt1s.com - CosmosPeople宇宙人想把你拍成一部電影MV官方完整版.mp3"
 	},
 	{
-		name:"魚丁糸 oaeen【Sorry 青春 Sorry for Youth】",
-		artist:"魚丁糸",
-		src:"music/y2mate.com - 魚丁糸 oaeenSorry 青春 Sorry for Youth Official Music Video.mp3"
+		name:"好不容易",
+		artist:"告五人",
+		src:"music/yt1s.com - 告五人 Accusefive  好不容易 Finally  Official Music Video   戲劇華燈初上片尾曲 .mp3"
 	},
 	{
-		name:"Forget Me Not feat. Dorian",
-		artist:"Dorian",
-		src:"music/y2mate.com - Andra  Forget Me Not feat Dorian Official Video.mp3"
-	},
-	{
-		name:"沒什麼大不了",
-		artist:"陳芳語",
-		src:"music/y2mate.com - 沒什麼大不了 no biggie feat 茄子蛋阿斌  Kimberley Chen 陳芳語Official Music Video.mp3"
-	},
-	{
-		name:"魏嘉瑩 Arrow Wei【要怎麼告訴你我多喜歡】",
-		artist:"魏嘉瑩 Arrow Wei",
-		src:"music/y2mate.com - 魏嘉瑩 Arrow Wei要怎麼告訴你我多喜歡feat 理想混蛋雞丁 Official Music Video.mp3"
-	},
-	{
-		name:"錘娜麗莎 - 我太笨",
-		artist:"錘娜麗莎",
-		src:"music/y2mate.com - 錘娜麗莎  我太笨是不是我太笨 只要你說的我全都信任不能去拆穿的又何必追問動態歌詞VietsubPinyin Lyrics.mp3"
-	},
-	{
-		name:"徐佳瑩 LaLa【雛形 Prototype】",
-		artist:"徐佳瑩 LaLa",
-		src:"music/y2mate.com - 徐佳瑩 LaLa雛形 PrototypeOfficial Lyrics Video.mp3"
-	},
-	{
-		name:"今天，半永久",
-		artist:"梁靜茹 Fish Leong",
-		src:"music/y2mate.com - 梁靜茹 Fish Leong今天半永久 Today  Halfway to The Permanent Official Music Video.mp3"
-	},
-	{
-		name:"Andra - Love Can Save It All",
-		artist:"Andra",
-		src:"music/y2mate.com - Andra  Love Can Save It All Official Video.mp3"
-	},
-	{
-		name:"Andra - Love Can Save It All",
-		artist:"Andra",
-		src:"music/y2mate.com - Andra  Love Can Save It All Official Video.mp3"
-	},
-	{
-		name:"九澤CP【最後一秒鐘 Last Second】",
-		artist:"九澤CP",
-		src:"music/y2mate.com - 九澤CP最後一秒鐘 Last Second電視劇無神之地不下雨片頭曲 Official Music Video.mp3"
-	},
-	{
-		name:"最悲傷的事 More Than Sorrow ",
-		artist:"A-Lin",
-		src:"music/y2mate.com - ALin 最悲傷的事 More Than Sorrow Official Music Video  比悲傷更悲傷的故事  影集版  主題曲.mp3"
-	},
-	{
-		name:"盧廣仲 Crowd Lu 自我的介紹 ",
-		artist:"盧廣仲 Crowd Lu",
-		src:"music/y2mate.com - 盧廣仲 Crowd Lu自我的介紹 in peaceOfficial Music Video.mp3"
-	},
-	{
-		name:"艾怡良 Eve Ai 貪 Greediness",
-		artist:"艾怡良 Eve Ai",
-		src:"music/y2mate.com - 艾怡良 Eve Ai 貪 GreedinessOfficial Music Video.mp3"
-	},
-	{
-		name:"Andra & Mara - Sweet Dreams",
-		artist:"Andra & Mara",
-		src:"music/y2mate.com - Andra  Mara  Sweet Dreams Radio Killer Remix.mp3"
-	},
-	{
-		name:"精神分裂 Split - Julia Wu 吳卓源",
-		artist:"吳卓源",
-		src:"music/y2mate.com - 精神分裂 Split  Julia Wu 吳卓源Official Music Video.mp3"
-	},
-	{
-		name:"理想混蛋 Bestards我就想你 Miss You So",
-		artist:"理想混蛋 Bestards",
-		src:"music/y2mate.com - 理想混蛋 Bestards我就想你 Miss You SoOfficial Music Video.mp3"
-	},
-	{
-		name:"玻璃心 黃明志 Namewee Ft.陳芳語",
-		artist:"黃明志",
-		src:"music/y2mate.com - 聽見玻璃心碎的聲音玻璃心 Fragile黃明志 Namewee Ft陳芳語 Kimberley Chen 鬼才做音樂 2021 Ghosician.mp3"
-	},
-	{
-		name:"孫盛希 Shi Shi 雨不停。流",
-		artist:"孫盛希",
-		src:"music/y2mate.com - 孫盛希 Shi Shi雨不停流 Never Ending Rain電視劇無神之地不下雨片尾曲 Official Music Video.mp3"
-	},
-	{
-		name:"寂寞讓你更快樂",
-		artist:"陳奕迅",
-		src:"music/y2mate.com - 陳奕迅寂寞讓你更快樂國OT大開眼戒MV.mp3"
-	},
-	{
-		name:"孤勇者",
-		artist:"陳奕迅",
-		src:"music/y2mate.com - 孤勇者英雄聯盟雙城之戰動畫劇集中文主題曲陳奕迅 Eason Chan Official MV.mp3"
-	},
-	{
-		name:"你瞞我瞞",
-		artist:"陳柏宇 Jason Chan",
-		src:"music/y2mate.com - 陳柏宇 Jason Chan  你瞞我瞞 Official MV.mp3"
-	},
-	{
-		name:"烏雲日記",
-		artist:"艾薇",
-		src:"music/y2mate.com - 烏雲日記.mp3"
-	},
-	{
-		name:"范瑋琪 FanFan【恰如其分的自己 Solitary Moment】",
-		artist:"范瑋琪",
-		src:"music/y2mate.com - 范瑋琪 FanFan恰如其分的自己 Solitary MomentOfficial MV.mp3"
-	},
-	{
-		name:"信爱成瘾",
-		artist:"Ella 陳嘉樺",
-		src:"music/y2mate.com - 信爱成瘾 Love Addiction Ella 陳嘉樺.mp3"
-	},
-	{
-		name:"Reunite · Jordie Power",
-		artist:"Jordie Power",
-		src:"music/y2mate.com - Reunite.mp3"
-	},
-	{
-		name:"Collage蓮花空行身染愛· 珂拉琪 Collage",
-		artist:"珂拉琪 Collage",
-		src:"music/y2mate.com - 珂拉琪 Collage蓮花空行身染愛.mp3"
-	},
-	{
-		name:"萬千花蕊慈母悲哀· 珂拉琪 Collage",
-		artist:"珂拉琪 Collage",
-		src:"music/y2mate.com - 珂拉琪 Collage萬千花蕊慈母悲哀.mp3"
-	},
-	{
-		name:"葬予規路火烌猶在· 珂拉琪 Collage",
-		artist:"珂拉琪 Collage",
-		src:"music/y2mate.com - 珂拉琪 Collage葬予規路火烌猶在.mp3"
-	},
-	{
-		name:"這該死的拘執佮愛· 珂拉琪 Collage",
-		artist:"珂拉琪 Collage",
-		src:"music/y2mate.com - 珂拉琪 Collage這該死的拘執佮愛.mp3"
-	},
-	{
-		name:"ozi - MISSIONARY",
-		artist:"ozi",
-		src:"music/y2mate.com - MISSIONARY 傳教士.mp3"
-	},
-	{
-		name:"ozi - Bad Intentions",
-		artist:"ozi",
-		src:"music/y2mate.com - Bad Intentions 企圖.mp3"
-	},
-	{
-		name:"ozi - 003",
-		artist:"ozi",
-		src:"music/y2mate.com - ØZI 003 Audio.mp3"
-	},
-	{
-		name:"何瑞康 - 刑者",
-		artist:"何瑞康",
-		src:"music/y2mate.com - 何瑞康  刑者 歌词版.mp3"
-	},
-	{
-		name:"TRASH 重感情的廢物 Heavyhearted Loser",
-		artist:"TRASH",
-		src:"music/y2mate.com - TRASH重感情的廢物 Heavyhearted LoserOfficial Music Video.mp3"
-	},
-	{
-		name:"TRASH 終究還是因為愛",
-		artist:"TRASH",
-		src:"music/TRASH終究還是因為愛 LOVEOfficial Music Video.mp3"
-	},
-	{
-		name:"茄子蛋EggPlantEgg - 浪子回頭",
-		artist:"茄子蛋EggPlantEgg",
-		src:"music/茄子蛋EggPlantEgg  浪子回頭 Back Here Again Official Music Video.mp3"
-	},
-	{
-		name:"茄子蛋EggPlantEgg - 愛情你比我想的閣較偉大",
-		artist:"茄子蛋EggPlantEgg",
-		src:"music/茄子蛋EggPlantEgg  愛情你比我想的閣較偉大 Official Music Video當男人戀愛時電影主題曲.mp3"
-	},
-	{
-		name:"阿冗 - 未發送",
-		artist:"阿冗",
-		src:"music/y2mate.com - 阿冗  未發送對話框裡的內容我輸入 刪除了再刪除一個人的交流我重複了又重複動態歌詞Lyrics.mp3"
-	},
-	{
-		name:"梁心頤 再也沒有你",
-		artist:"梁心頤",
-		src:"music/y2mate.com - Lara Liang 梁心頤  No More U 再也沒有你  Official MV.mp3"
-	},
-	{
-		name:"陳忻玥 Vicky Chen - 煙幕",
-		artist:"陳忻玥",
-		src:"music/y2mate.com - 陳忻玥 Vicky Chen  煙幕 Smokescreen 彩虹六部曲第一次La Boum Official MV  KKTV原創電視劇.mp3"
-	},
-	{
-		name:"Westlife - 平凡之路",
-		artist:"Westlife",
-		src:"music/y2mate.com - 西城男孩westlife全球首场直播平凡之路完整版.mp3"
-	},
-	{
-		name:" Westlife - My Hero",
-		artist:"Westlife",
-		src:"music/y2mate.com - Westlife  My Hero Official Visualiser.mp3"
-	},
-	{
-		name:" Anthem Lights & Charlotte Ave",
-		artist:"Anthem Lights & Charlotte Ave",
-		src:"music/y2mate.com - AMERICA MEDLEY 2021  Anthem Lights  Charlotte Ave.mp3"
-	},
-	{
-		name:"The Kid Laroi - Stay ft. Justin Bieber & Juice WRLD",
-		artist:"Justin Bieber & Juice WRLD",
-		src:"music/y2mate.com - 中文翻譯The Kid Laroi  Stay ft Justin Bieber  Juice WRLD.mp3"
-	},
-	{
-		name:"Ed Sheeran - Shivers",
-		artist:"Ed Sheeran",
-		src:"music/y2mate.com - Ed Sheeran  Shivers中文歌詞字幕Lyrics.mp3"
-	},
-	{
-		name:"Ed Sheeran - Bad Habits",
-		artist:"Ed Sheeran",
-		src:"music/y2mate.com - 我壞習慣的後果是你Ed Sheeran  Bad Habits 英繁中字.mp3"
-	},	
-	{
-		name:"BTS-Dynamite -for CC ",
-		artist:"BTS ",
-		src:"music/y2mate.com - BTS 방탄소년단 Dynamite Official MV.mp3"
-	},	
-	{
-		name:"Dan + Shay, Justin Bieber-10,000 Hours -for CC ",
-		artist:"Dan + Shay, Justin Bieber ",
-		src:"music/y2mate.com - Dan  Shay Justin Bieber  10000 Hours Official Music Video.mp3"
-	},	
-	{
-		name:"88rising-California -for BH ",
-		artist:"88rising ",
-		src:"music/y2mate.com - Rich Brian NIKI  Warren Hue  California Official Music Video.mp3"
-	},	
-	{
-		name:"Alex Goot - Pretty Eyes ",
-		artist:"Alex Goot ",
-		src:"music/yt1s.com - Pretty Eyes    Alex Goot 中文字幕.mp3"
-	},	
-	{
-		name:"Vanessa Carlton - A Thousand Miles  ",
-		artist:"Vanessa Carlton ",
-		src:"music/yt1s.com - Vanessa Carlton  A Thousand Miles Official Video.mp3"
-	},
-	{
-		name:"Keira Knightley - Tell Me If You Wanna Go Home ",
-		artist:"Keira Knightley ",
+		name:"Tell Me If You Wanna Go Home Begin Again",
+		artist:"Keira Knightley",
 		src:"music/yt1s.com - Keira Knightley  Tell Me If You Wanna Go Home Begin Again Soundtrack  Interscope.mp3"
 	},
 	{
-		name:"Passenger - Let Her Go ",
-		artist:"Passenger ",
-		src:"music/yt1s.com - Passenger  Let Her Go Official Video.mp3"
-	},	
+		name:"Faded",
+		artist:"Alan walker",
+		src:"music/yt1s.com -  Faded人間迷走  Alan Walker feat Iselin Solheim 現場版中文字幕.mp3"
+	},
 	{
-		name:"Adam Levine - Lost Stars ",
-		artist:"Adam Levine ",
+		name:"Lost Stars from Begin Again",
+		artist:"Adam Levine",
 		src:"music/yt1s.com - Adam Levine  Lost Stars from Begin Again.mp3"
-	},	
+	},
 	{
-		name:"Against The Current & ONE OK ROCK - Dreaming Alone ",
-		artist:"Against The Current & ONE OK ROCK",
+		name:"Catch My Breath",
+		artist:"ATC",
+		src:"music/yt1s.com - Catch My Breath Alex Goot  Against The Current Cover 中文字幕.mp3"
+	},
+	{
+		name:"A Sky Full Of Stars",
+		artist:"Coldplay",
+		src:"music/yt1s.com - Coldplay  A Sky Full Of Stars Official Video.mp3"
+	},
+	{
+		name:"The Scientist",
+		artist:"Coldplay",
+		src:"music/yt1s.com - Coldplay  The Scientist Official Video.mp3"
+	},
+	{
+		name:"Viva La Vida",
+		artist:"Coldplay",
+		src:"music/yt1s.com - Coldplay  Viva La Vida Official Video.mp3"
+	},
+	{
+		name:"Yellow",
+		artist:"Coldplay",
+		src:"music/yt1s.com - Coldplay  Yellow  黃色   中文字幕 Lyrics.mp3"
+	},
+	{
+		name:"Dreaming Alone",
+		artist:"ONE OK ROCK",
 		src:"music/yt1s.com - Dreaming Alone  Against The Current feat  Taka from ONE OK ROCK 中文字幕.mp3"
 	},
 	{
-		name:"Linkin Park - In The End ",
-		artist:"Linkin Park",
-		src:"music/yt1s.com - In The End 終點  Linkin Park 聯合公園 中文歌詞.mp3"
-	},			
-	{
-		name:"The Chainsmokers & Coldplay  - Something Just Like This ",
-		artist:"The Chainsmokers & Coldplay ",
-		src:"music/yt1s.com - 拯救了整座城市卻救不了你Something Just Like This 如此而已  The Chainsmokers  Coldplay 剪輯版 蜘蛛人驚奇再起 中文字幕.mp3"
-	},	
-	{
-		name:"The Cab - Angel With A Shotgun ",
-		artist:"The Cab",
-		src:"music/yt1s.com - 歐美電影剪輯燃燒吧英雄魂Angel With A Shotgun中文字幕.mp3"
-	},	
-	{
-		name:"Ashes Remain - On My Own ",
-		artist:"Ashes Remain",
-		src:"music/yt1s.com - 歐美電影剪輯3最大的敵人是我們的內心燃燃燃 中文字幕.mp3"
-	},	
-	{
-		name:"Coldplay - Yellow  ",
-		artist:"Coldplay",
-		src:"music/yt1s.com - Coldplay  Yellow  黃色   中文字幕 Lyrics.mp3"
-	},	
-	{
-		name:"Coldplay - The Scientist  ",
-		artist:"Coldplay",
-		src:"music/yt1s.com - Coldplay  The Scientist Official Video.mp3"
-	},	
-	{
-		name:"Coldplay - A Sky Full Of Stars  ",
-		artist:"Coldplay",
-		src:"music/yt1s.com - Coldplay  A Sky Full Of Stars Official Video.mp3"
-	},	
-	{
-		name:"Coldplay - Viva La Vida  ",
-		artist:"Coldplay",
-		src:"music/yt1s.com - Coldplay  Viva La Vida Official Video.mp3"
-	},	
-	{
-		name:"The Band Perry - If I Die Young  ",
-		artist:"The Band Perry",
-		src:"music/yt1s.com - The Band Perry  If I Die Young Official Video.mp3"
-	},	
-	{
-		name:"Hawk Nelson - Sold Out  ",
-		artist:"Hawk Nelson",
-		src:"music/yt1s.com - 歐美電影剪輯5Manners Makyth Man 不知禮無以立也燃.mp3"
-	},	
-	{
-		name:"Alex Goot & Against The Current - Catch My Breath   ",
-		artist:"Alex Goot & Against The Current",
-		src:"music/yt1s.com - Catch My Breath Alex Goot  Against The Current Cover 中文字幕.mp3"
-	},	
-	{
-		name:"Alan Walker - Faded ",
-		artist:"Alan Walker",
-		src:"music/yt1s.com -  Faded人間迷走  Alan Walker feat Iselin Solheim 現場版中文字幕.mp3"
-	},	
-	{
-		name:"Against the Current - Something You Need Acoustic   ",
-		artist:"Against the Current ",
-		src:"music/yt1s.com - Something You Need Acoustic   Against the Current 中文字幕.mp3"
-	},
-	{
-		name:"Against the Current - Brighter    ",
-		artist:"Against the Current ",
-		src:"music/yt1s.com - Brighter   Against the Current 中文字幕.mp3"
-	},
-	{
-		name:"Against the Current - Outsiders     ",
-		artist:"Against the Current ",
-		src:"music/yt1s.com - Outsiders 局外人  Against The Current Lyrics Video 中文字幕.mp3"
-	},
-	{
-		name:"OneRepublic - Counting Stars    ",
-		artist:"OneRepublic  ",
+		name:"Counting Stars",
+		artist:"OneRepublic",
 		src:"music/yt1s.com - OneRepublic  Counting Stars Official Music Video.mp3"
-	},	
+	},
+	{
+		name:"Angel With A Shotgun",
+		artist:"匿名",
+		src:"music/yt1s.com - 歐美電影剪輯燃燒吧英雄魂Angel With A Shotgun中文字幕.mp3"
+	},
+	{
+		name:"愛情你比我想的閣較偉大",
+		artist:"茄子蛋",
+		src:"music/茄子蛋EggPlantEgg  愛情你比我想的閣較偉大 Official Music Video當男人戀愛時電影主題曲.mp3"
+	},
+	{
+		name:"Something Just Like This",
+		artist:"The Chainsmokers  Coldplay",
+		src:"music/yt1s.com - 拯救了整座城市卻救不了你Something Just Like This 如此而已  The Chainsmokers  Coldplay 剪輯版 蜘蛛人驚奇再起 中文字幕.mp3"
+	},
 ];
